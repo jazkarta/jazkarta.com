daa0be33234c3bb78053b15dca025c99f514911f
cewing <cris@crisewing.com>
2015-04-06 10:36:25 -0700

    add contributors, remove unused dexterity readme fix setup.py

--- src/jazkarta.contenttypes/setup.py
+++ src/jazkarta.contenttypes/setup.py
@@ -14,7 +14,7 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='jaz.contenttypes',
+setup(name='jazkarta.contenttypes',
       version=version,
       description="Content types for the jazkarta web site",
       long_description=long_description,
@@ -28,11 +28,11 @@ setup(name='jaz.contenttypes',
       keywords='',
       author='',
       author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      url='https://github.com/jazkarta/jazkarta.com',
       license='gpl',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-      namespace_packages=['jaz'],
+      namespace_packages=['jazkarta'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
