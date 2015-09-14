825d71e0a7b1086f4ec13e8d41cec00248d4b437
cewing <cris@crisewing.com>
2015-09-14 15:06:46 -0700

    fix services vocabulary to provide values based on the content of the designated folder in the site
    Addresses #28

--- src/jazkarta.contenttypes/src/jazkarta/contenttypes/vocabularies.py
+++ src/jazkarta.contenttypes/src/jazkarta/contenttypes/vocabularies.py
@@ -1,3 +1,4 @@
+import os.path
 from plone import api
 from plone.registry.interfaces import IRegistry
 from Products.CMFCore.utils import getToolByName
@@ -31,20 +32,6 @@ def industries(context):
 alsoProvides(industries, IContextSourceBinder)
 
 
-def services(context):
-    registry = queryUtility(IRegistry)
-    jaz_settings = registry.forInterface(IJazSettings)
-    terms = []
-    for service in jaz_settings.services:
-        terms.append(
-            SimpleVocabulary.createTerm(
-                service, service.encode('utf-8'), service
-            )
-        )
-    return SimpleVocabulary(terms)
-alsoProvides(services, IContextSourceBinder)
-
-
 def team_members(context):
     catalog = api.portal.get_tool('portal_catalog')
     team_member_brains = catalog(portal_type='jazkarta.team_member')
@@ -73,3 +60,31 @@ def case_studies(context):
         ))
     return SimpleVocabulary(terms)
 alsoProvides(case_studies, IContextSourceBinder)
+
+
+def services(context):
+    registry = queryUtility(IRegistry)
+    jaz_settings = registry.forInterface(IJazSettings)
+    site_path = '/'.join(
+        api.portal.get_navigation_root(context).getPhysicalPath()
+    )
+    services_path = jaz_settings.services_location
+    search_path = os.path.join(site_path, services_path.lstrip('/'))
+
+    terms = []
+    catalog = api.portal.get_tool('portal_catalog')
+    # all published documents at the root level in our designated folder
+    results = catalog(
+        path={'query': search_path, 'depth': 1},
+        portal_type="Document",
+        review_state="published"
+    )
+    for brain in results:
+        token = brain.getPath()
+        terms.append(SimpleTerm(
+            value=brain.UID,
+            token=token,
+            title=brain.Title.decode('utf-8')
+        ))
+    return SimpleVocabulary(terms)
+alsoProvides(services, IContextSourceBinder)
