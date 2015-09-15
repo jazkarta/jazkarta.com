3713c65277394a52640224280473d9599a9243b2
cewing <cris@crisewing.com>
2015-09-15 15:46:48 -0700

    provide collection criteria and vocabularies to feed them
    Addresses #38

--- src/jazkarta.contenttypes/src/jazkarta/contenttypes/vocabularies.py
+++ src/jazkarta.contenttypes/src/jazkarta/contenttypes/vocabularies.py
@@ -1,10 +1,10 @@
 import os.path
 from plone import api
+from plone.app.vocabularies.catalog import KeywordsVocabulary
 from plone.registry.interfaces import IRegistry
-from Products.CMFCore.utils import getToolByName
 from zope.component import queryUtility
-from zope.interface import alsoProvides
-from zope.schema.interfaces import IContextSourceBinder
+from zope.interface import alsoProvides, implementer
+from zope.schema.interfaces import IContextSourceBinder, IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from jazkarta.contenttypes.interfaces import IJazSettings
@@ -21,6 +21,11 @@ def technologies(context):
 alsoProvides(technologies, IContextSourceBinder)
 
 
+class TechnologyVocabulary(KeywordsVocabulary):
+    keyword_index = 'technology'
+TechnologyVocabularyFactory = TechnologyVocabulary()
+
+
 def industries(context):
     registry = queryUtility(IRegistry)
     jaz_settings = registry.forInterface(IJazSettings)
@@ -32,19 +37,9 @@ def industries(context):
 alsoProvides(industries, IContextSourceBinder)
 
 
-def team_members(context):
-    catalog = api.portal.get_tool('portal_catalog')
-    team_member_brains = catalog(portal_type='jazkarta.team_member')
-    terms = []
-    for brain in team_member_brains:
-        token = brain.getPath()
-        terms.append(SimpleTerm(
-            value=brain.UID,
-            token=token,
-            title=brain.Title.decode('utf8')
-            ))
-    return SimpleVocabulary(terms)
-alsoProvides(team_members, IContextSourceBinder)
+class IndustryVocabulary(KeywordsVocabulary):
+    keyword_index = 'industry'
+IndustryVocabularyFactory = IndustryVocabulary()
 
 
 def case_studies(context):
@@ -88,3 +83,49 @@ def services(context):
         ))
     return SimpleVocabulary(terms)
 alsoProvides(services, IContextSourceBinder)
+
+
+@implementer(IVocabularyFactory)
+class ServiceVocabulary(object):
+    keyword_index = 'services'
+
+    def __call__(self, context, query=None):
+        self.catalog = api.portal.get_tool('portal_catalog')
+        if self.catalog is None:
+            return SimpleVocabulary([])
+        index = self.catalog._catalog.getIndex(self.keyword_index)
+
+        def safe_encode(term):
+            if isinstance(term, unicode):
+                # no need to use portal encoding for transitional encoding from
+                # unicode to ascii. utf-8 should be fine.
+                term = term.encode('utf-8')
+            return term
+
+        brains = self.catalog(UID=tuple(index._index))
+        items = [
+            SimpleTerm(brain.UID, brain.getPath(), safe_encode(brain.Title))
+            for brain in brains
+        ]
+        return SimpleVocabulary(items)
+ServiceVocabularyFactory = ServiceVocabulary()
+
+
+def team_members(context):
+    catalog = api.portal.get_tool('portal_catalog')
+    team_member_brains = catalog(portal_type='jazkarta.team_member')
+    terms = []
+    for brain in team_member_brains:
+        token = brain.getPath()
+        terms.append(SimpleTerm(
+            value=brain.UID,
+            token=token,
+            title=brain.Title.decode('utf8')
+            ))
+    return SimpleVocabulary(terms)
+alsoProvides(team_members, IContextSourceBinder)
+
+
+class TeamVocabulary(ServiceVocabulary):
+    keyword_index = 'team'
+TeamVocabularyFactory = TeamVocabulary()
