aec10fc098116acdf09f2b934fd8beb30f05de96
Alec Mitchell <alecpm@gmail.com>
2015-09-22 23:18:06 -0700

    Update styles for footer/header. Add js for stretching full-width images.

---
+++ src/jazkarta.plonetheme/src/jazkarta/plonetheme/theme/jazkarta_theme.js
@@ -0,0 +1,11 @@
+jQuery(function($) {
+    function adjust_full_width() {
+        var window_width = $(window).width();
+        var $rows = $('.row.tile-fullwidth');
+        var width = $rows.outerWidth(true);
+        var new_margin = -(window_width - width)/2;
+        $rows.css('margin-left', new_margin).css('margin-right', new_margin);
+    }
+    adjust_full_width();
+    $(window).resize(adjust_full_width);
+});
